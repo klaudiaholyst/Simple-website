b1dfd37a21af7daadde9f1f557b80a13bad64da2
Klaudia <kklaudiaw@gmail.com>
2019-11-05 21:44:53 +0100

    mobile attributes,  nav img, definition-list

--- scripts/script.js
+++ scripts/script.js
@@ -2,14 +2,20 @@
 const menuButton = document.getElementById('menu-button');
 const menu = document.querySelector('.menu');
 const menuItems = document.querySelectorAll('.menu__item');
-const checkBox = document.getElementById('nav-checkbox');
 const nav = document.getElementById('nav');
-
+const MenuButtonImg = document.getElementById('menu-button__img');
 // SHOW OR HIDE MENU ON MOBILE
 
 function showOrHide() {
     menu.classList.toggle('active');
     menuItems.forEach(item => item.classList.toggle('active-link'));
+    if (menu.classList.contains('active')){
+        menuButton.setAttribute("aria-expanded", 'true');
+        MenuButtonImg.setAttribute("src", "img/times-solid.svg");
+    } else{
+        menuButton.setAttribute("aria-expanded", 'false');
+        MenuButtonImg.setAttribute("src", "img/bars.svg")
+    }
 }
 menuButton.addEventListener('click', showOrHide);
 
@@ -25,5 +31,15 @@ menu.addEventListener("click", function (ev) {
         showOrHide();
     }
 }, false);
+
+
+
+//DELETING BUTTON ATTRIBUTE ON MOBILE 
+
+if ($(window).width() < 756){
+    menuButton.removeAttribute('aria-haspopup')
+} else{
+    menuButton.setAttribute('aria-haspopup=true')
+}
 
 }
